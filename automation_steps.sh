e94c8c5e99e411b7551412cb5c1bcc47b9970935
chameleonlabs2021 <96870965+chameleonlabs2021@users.noreply.github.com>
2022-01-30 10:51:57 +0800

    Add files via upload

---
+++ automation_steps.sh
@@ -0,0 +1,11 @@
+#docker pull jupyter/pyspark-notebook
+imageid=`docker images|grep jupyter/pyspark-notebook|awk -F ' ' 'NR==1{print $3}'`
+nohup docker run -p 8888:8888 $imageid & docker.out 2>&1
+# docker exec -it 3d293204ab1b /bin/bash
+sleep 15
+a=`docker ps|awk -F ' ' '{print $1}'`
+con=`echo $a|awk -F ' ' '{print $2}'`
+docker cp worldcities_sample.csv ${con}:/home/jovyan/
+docker cp city_temperature_sample.csv ${con}:/home/jovyan/
+docker cp city_temperature_sample.csv ${con}:/home/jovyan/
+
